86adc5bbaece3aa97702965492e49782eba61456
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-01-28 12:41:33 +0100

    index.php: make rss scanning only keep the longster podcast per date

--- index.php
+++ index.php
@@ -1,28 +1,45 @@
 <?php
-
 $xmlDoc = new DOMDocument;
 $xmlDoc->load($_GET['url']) or die("Error: Cannot create object");
-
-if (preg_match('/10076/', $_GET['url'])) {
-    print $xmlDoc->saveXML();
-} else {
-  $xml = $xmlDoc->documentElement;
-  $arr = $xml->getElementsByTagName('item');
-  $domElemsToRemove = array();
-  foreach ($arr as $item) { // DOMElement Object
-      $enclosure = $item->getElementsByTagName('enclosure')->item(0);
-      $url =  $enclosure->getAttribute('url');
-      $is_complet = preg_match('/\-0\.mp3$/', $url);
-      if (preg_match('/rss_10212/', $_GET['url'])) {
-        $is_complet = preg_match('/\-6\.mp3$/', $url);
-      }
-      if (! $is_complet) {
-          $domElemsToRemove[] = $item;
-      }
-
-  }
-  foreach ( $domElemsToRemove as $domElement ) {
-    $domElement->parentNode->removeChild($domElement);
-  }
-  print $xmlDoc->saveXML();
+$xml = $xmlDoc->documentElement;
+$arr = $xml->getElementsByTagName('item');
+$domElemsToRemove = array();
+$domElemsToEval = array();
+$lastDate = "";
+unset($lastDate);
+$maxLenght = 0;
+foreach ( $arr as $item ) { // DOMElement Object
+    $enclosure = $item->getElementsByTagName('enclosure')->item(0);
+    $length = $enclosure->getAttribute('length');
+    $length = intval($length);
+    $pubdate = $item->getElementsByTagName('pubDate')->item(0)->nodeValue;
+    $date = preg_replace(
+        '/(.*) \d{2}:\d{2}:\d{2} .*/',
+        '$1',
+        $pubdate
+    );
+    $date = strval($date);
+    if ( isset($lastDate) && strcmp($lastDate, $date) !== 0 ) {
+        foreach ( $domElemsToEval as $itemToEval ) {
+            $enclosureToEval = $itemToEval->getElementsByTagName('enclosure')->item(0);
+            $lengthToEval = $enclosureToEval->getAttribute('length');
+            $lengthToEval = intval($lengthToEval);
+            if ( $lengthToEval < $maxLenght ) {
+                $domElemsToRemove[] = $itemToEval;
+            }
+        }
+        $maxLenght = 0;
+        unset($domElemsToEval);
+        $domElemsToEval = array();
+    }
+    $lastDate = $date;
+    if ( $length > $maxLenght ) {
+        $maxLenght = $length;
+    }
+    $domElemsToEval[] = $item;
 }
+foreach ( $domElemsToRemove as $domElement ) {
+    $domElement->parentNode->removeChild($domElement);
+}
+print $xmlDoc->saveXML();
+?>
